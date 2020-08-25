2fb56ba207ef031a0ffe9a0317aa1304fb9f2de8
Mokesh-mock <mokeshchitti@gmail.com>
2020-08-25 16:59:07 +0530

    notes

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ mongoose.connect(dbConfig.url, {
 
 // define a simple route
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes.' })
+  res.json({ message: 'Mokesh notes application' })
 })
 
 require('./app/routes/note.routes.js')(app)
